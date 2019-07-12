279fba5c82e5ab38814afc0177e0012c52d624d7
Maxim Savuskin <m.savuskin@tarent.de>
2019-07-12 14:23:50 +0200

    remove wrong parameter

--- src/app/app.constants.ts
+++ src/app/app.constants.ts
@@ -1,5 +1,4 @@
-export const BASE_URL = 'http://socialwall.lan.tarent.de:12300';
-// export const BASE_URL = 'http://localhost:12300';
+export const BASE_URL = 'http://localhost:12300';
 export const URL_LOGIN = BASE_URL + '/login';
 export const URL_SOCIAL_POST = BASE_URL + '/socialposts';
 export const URL_CUSTOM_POST = BASE_URL + '/customposts';
